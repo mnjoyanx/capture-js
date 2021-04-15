746dcd34b53f9c1d6fa4d2387971ccd228e66a7d
a-eliza <eliza.aleksanyan94@gmail.com>
2021-04-15 12:57:49 +0400

    stop capturing

--- modal.js
+++ modal.js
@@ -1,4 +1,18 @@
+let videoSelector = document.getElementById('videoplayer')
+const modalWrapper = document.querySelector('.modal-wrapper')
+const timeSelector = document.getElementById('timer')
+const controller = document.getElementById('controller')
+const stopRecordingSelector = document.getElementById('stoprecording')
+const startRecordingSelector = document.getElementById('startrecording')
+
+// timer 
+const minuteSelector = document.getElementById('minute')
+const timerFirst = document.getElementById('first')
+const timerSecond = document.getElementById('second')
+
+
 export const modal = () => {
+
     let check = false
 
     const modalSelector = document.getElementById('modal'),
@@ -14,21 +28,26 @@ export const modal = () => {
     startRecordignSelector.addEventListener('click', () => {
         if (check) {
             errMessage.classList.remove('show')
-            shareScreen()
+            startRecording()
         } else {
             errMessage.classList.add('show')
         }
     })
 
-    modalSelector.classList.add('show')
+    stopRecordingSelector.addEventListener('click', () => {
+        stopRecording()
+    })
+
+    modalSelector.classList.remove('hidden')
 }
 
 
-const shareScreen = async() => {
-    let captureStreem = null
+const startRecording = async() => {
     const options = {
         video: {
-            cursor: "always"
+            cursor: "always",
+            width: 450,
+            height: 300
         },
         audio: {
             echoCancellation: true,
@@ -38,8 +57,52 @@ const shareScreen = async() => {
     }
 
     try {
-        captureStreem = await navigator.mediaDevices.getUserMedia(options)
+        videoSelector.srcObject = await navigator.mediaDevices.getDisplayMedia(options)
+        videoSelector.onloadedmetadata = function() {
+            videoSelector.play();
+            modalWrapper.classList.add('hidden')
+            controller.classList.remove('hidden')
+            stopRecordingSelector.classList.remove('hidden')
+            startRecordingSelector.classList.add('hidden')
+            startRecordingTimer()
+        };
     } catch (err) {
         console.log(err, 'err messsage')
     }
+}
+
+const stopRecording = async() => {
+    let tracks = await videoSelector.srcObject.getTracks()
+
+    tracks.forEach(track => track.stop())
+
+    videoSelector.srcObject = null
+    videoSelector.classList.add('hidden')
+    startRecordingSelector.classList.remove('hidden')
+    stopRecordingSelector.classList.add('hidden')
+
+}
+
+const stopTimer = (e) => {
+
+    stopRecordingSelector.addEventListener('click', () => {
+        clearInterval(e)
+    })
+}
+
+const startRecordingTimer = () => {
+    let second = 0
+
+    let a = 0
+    let t = setInterval(() => {
+        second++
+        timerSecond.textContent = second
+        if (second > 9) {
+            second = 0
+            a++
+            timerFirst.textContent = a
+        }
+    }, 1000)
+    stopTimer(t)
+
 }
